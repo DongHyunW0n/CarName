c98135c43143f002e7395553484ba81f1ade14fc
DongHyunW0n <68365742+DongHyunW0n@users.noreply.github.com>
2022-08-17 13:13:08 +0900

    ㅁㄴㅇㅇㅇ

---
+++ CarName/Quotes.swift
@@ -0,0 +1,12 @@
+//
+//  Quotes.swift
+//  CarName
+//
+//  Created by WonDongHyun on 2022/08/17.
+//
+
+import Foundation
+struct Quote{
+    let contents: String
+    let name: String
+}
--- CarName/ViewController.swift
+++ CarName/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    let quotes = [
+        
+    Quote(contents: "코나", name: "현대"),
+    Quote(contents: "소나타", name: "현대"),
+    Quote(contents: "베뉴", name: "현대"),
+    Quote(contents: "타이칸", name: "포르쉐"),
+    Quote(contents: "무르시엘라고", name: "람보르기니")]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tabButton(_ sender: UIButton) {
+        
+        let random = Int(arc4random_uniform(5)) // 0에서 4 사이의 난수를 쏴줌
+        
+        
+        let quote = quotes[random]
+        self.quoteLabel.text = quote.contents
+        self.nameLabel.text = quote.name
+    }
+    
 }
 
